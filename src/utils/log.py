c19343d0bb12619dddaa70058becd9f425f9886d
oscar-sinaga <masterspeed90@mail.ugm.ac.id>
2025-04-17 16:31:56 +0700

    modified log script

--- src/utils/log.py
+++ src/utils/log.py
@@ -25,7 +25,7 @@ def read_etl_log(filter_params: dict):
     """
     try:
         # create connection to database
-        conn = log_engine()
+        # conn = log_engine()
         
         # To help with the incremental process, get the etl_date from the relevant process
         """
@@ -37,12 +37,22 @@ def read_etl_log(filter_params: dict):
             status = %s and
             process = %s
         """
-        query = sqlalchemy.text(read_sql(MODEL_PATH_LOG_ETL,"log"))
 
-        # Execute the query with pd.read_sql
-        df = pd.read_sql(sql=query, con=conn, params=(filter_params,))
+        with log_engine().connect() as conn:
+
+            # Load query
+            query = sqlalchemy.text(read_sql(MODEL_PATH_LOG_ETL, "log"))
+
+            # Execute the query
+            df = pd.read_sql(sql=query, con=conn, params=filter_params)
+
+            return df
+        # query = sqlalchemy.text(read_sql(MODEL_PATH_LOG_ETL,"log"))
+
+        # # Execute the query with pd.read_sql
+        # df = pd.read_sql(sql=query, con=conn, params=(filter_params,))
 
         #return extracted data
-        return df
+        # return df
     except Exception as e:
         print("Can't execute your query. Cause: ", str(e))
