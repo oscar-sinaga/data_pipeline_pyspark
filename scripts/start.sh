e0ceeec3c3a5fe80142b4a36c7533c3f95853e38
oscar-sinaga <oscarmsinaga@gmail.com>
2025-06-24 06:54:37 +0700

    modified start.sh and start using docker exec -it spark-master  bash scripts/start.sh directly

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Tambahkan PYTHONPATH hanya untuk script ini
+export PYTHONPATH="/app:/app/src:/opt/bitnami/spark/python/"
+
 LOG_DIR="/app/logs"
 mkdir -p "$LOG_DIR"
 
 LOG_FILE="$LOG_DIR/pipeline_$(date +%Y%m%d).log"
 echo "[INFO] Starting pipeline at $(date)" >> "$LOG_FILE"
+echo "[INFO] PYTHONPATH=$PYTHONPATH" >> "$LOG_FILE"
 
-# Jalankan pipeline dan simpan stdout + stderr ke log
+# Jalankan pipeline
 python /app/pipeline.py >> "$LOG_FILE" 2>&1
 
 echo "[INFO] Finished pipeline at $(date)" >> "$LOG_FILE"
